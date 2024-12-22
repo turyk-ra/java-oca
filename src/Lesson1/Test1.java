f3a321dcde2c3bfc5417c6c1dc1e801f613ae01a
Raman Turyk <roman.turik48@gmail.com>
2024-12-22 14:20:20 +0300

    first lesson

---
+++ src/Lesson1/Test1.java
@@ -0,0 +1,9 @@
+package Lesson1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.print("Welcome!!!");
+        System.out.println("Welcome2!!!");
+        System.out.print("Hello");
+    }
+}
